2bae0006aa8f067f590197304484a950fb207a0c
yz <liuyz@powerinfo.net>
2019-09-29 10:06:17 +0800

    add socketIO&Starscream & init sdk

--- iLiveDemo/AppDelegate.m
+++ iLiveDemo/AppDelegate.m
@@ -8,16 +8,34 @@
 
 #import "AppDelegate.h"
 
+#import <PI_iRoom/PIiRoom.h>
+//#import <PI_iRoom/PIiOSLogger.h>
+
 @interface AppDelegate ()
 
 @end
 
 @implementation AppDelegate
 
+void uncaughtExceptionHandler(NSException* exception) {
+    NSLog(@"CRASH: %@", exception);
+    NSLog(@"Stack Trace: %@", [exception callStackSymbols]);
+}
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    
+    NSSetUncaughtExceptionHandler(&uncaughtExceptionHandler);
+    
+    [self config];
+    
     // Override point for customization after application launch.
     return YES;
 }
+-(void)config{
 
+//    [PIiOSLogger setLogToConsole:true];
+
+    NSString *logPath = NSTemporaryDirectory();
+    [PIiRoom initialize:@"iRoom" andNdSelect:999 andRSPort:80 andPISPort:81 andDomain:@"" andRSTimeoutThreshold:5000 andLogPath:logPath];
+}
 @end
